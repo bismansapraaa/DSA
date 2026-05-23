988a900d09d13011fbc5b171a20e48870836d7c9
Bisman Sapra <bismansapra@MacBook-Air.local>
2026-05-23 19:27:56 +0530

    Solved right triangle

---
+++ Day01_Basics/rightTriangle.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+     cin>> x;
+    for( int i=0; i<x-1; i++)
+    {        
+        cout<<"*";
+       
+        for(int j=1;j<x-1;j++)
+        {
+            if( i==j)
+            {
+                cout<<"*";
+            }
+            else 
+            {
+                cout<<" ";
+            }
+        }
+        
+         cout<<endl; 
+    
+    }
+    for(int i=0;i<x;i++){
+        cout<<"*";
+    }
+
+    return 0;
+}
