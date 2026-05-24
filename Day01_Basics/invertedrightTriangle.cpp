ad52af2148fe8d4421336f978ec765477e161d38
Bisman Sapra <bismansapra@MacBook-Air.local>
2026-05-24 11:02:21 +0530

    solved invertedrightTriangle

---
+++ Day01_Basics/invertedrightTriangle.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     int n = 5;
+    for(int i =0;i<n;i++)
+    {
+        cout<<"*";
+    }
+        cout<<endl;
+    for(int i=1;i<n;i++)
+    {
+        cout<<"*";
+        
+        for(int j=1; j<n;j++)
+        {
+            if(j==n-i-1)
+            {
+                cout<<"*";
+            }
+            else
+            {
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+     
+
+
+    return 0;
+}
